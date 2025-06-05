c93ae5cb7c95768706f00eb65c5ef78315ea1300
Rohit Chand <rohitchand490@gmail.com>
2025-06-05 18:26:15 +0530

    audio

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -2,6 +2,9 @@
 // export const SERVER_URI = "http://180.151.233.107:6969";
 // export const SERVER_URI = "https://survey-3uf0.onrender.com";
 export const SERVER_URI = "https://backend.bharatdemographic.com"
+export const SERVER_BUCKET = "https://survey-media.blr1.digitaloceanspaces.com"
+
+
 // users
 export const get_user = "api/user/getUser";
 export const get_all_users = "api/user/getAllUsers";
