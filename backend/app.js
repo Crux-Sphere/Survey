a7e1bc50a18cf7b02a635f4abdcd9fc27b8add21
Rohit Chand <rohitchand490@gmail.com>
2025-06-05 14:06:06 +0530

    new

--- backend/app.js
+++ backend/app.js
@@ -57,8 +57,8 @@ app.get("/api/health", (req, res) => {
 
 // app.use(express.static(path.join(__dirname, "../frontend/out/")));
 app.use('/uploads', express.static('uploads'));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "../frontend/out/index.html")),
-);
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../frontend/out/index.html")),
+// );
 
 module.exports = app;
